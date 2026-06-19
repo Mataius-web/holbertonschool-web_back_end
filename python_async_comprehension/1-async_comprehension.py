c7951d974fad63ecd8426e539be7b304a28584b9
Mataius-web <nicolas.jacquemin26@gmail.com>
2026-06-19 23:04:37 +0200

    fix

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""Module that collects random numbers using an async comprehension."""
+"""Async comprehension that collects random numbers."""
+
 from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Collect 10 random floats from async_generator
-    using an async comprehension."""
-    return [num async for num in async_generator()]
+    """Collect 10 random numbers using async comprehension."""
+    return [i async for i in async_generator()]
