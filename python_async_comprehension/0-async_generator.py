1d6088b44634d4969dbabd1a991426c85c9ef68d
Mataius-web <nicolas.jacquemin26@gmail.com>
2026-06-19 23:01:08 +0200

    fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,8 +6,8 @@ import random
 from typing import Generator
 
 
-async def asyncgenerator() -> Generator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yield a random number between 0 and 10 after waiting 1 second."""
-    for in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
